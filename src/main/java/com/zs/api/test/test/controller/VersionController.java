4ef5f49d4ee044cf71f33e529b15e7c0e48ca0fa
zhousheng <10499715888@qq.com>
2022-05-03 23:20:39 +0800

    131564

---
+++ src/main/java/com/zs/api/test/test/controller/VersionController.java
@@ -0,0 +1,14 @@
+package com.zs.api.test.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+    @GetMapping("/version")
+    public String getVersion(){
+        return "1.0";
+    }
+
+}
